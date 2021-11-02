47b1b4276da3fc37ef2d9e4dbb1cce899df6e513
newjohn <newjohn0518@naver.com>
2021-11-02 22:31:00 +0900

    models

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from core import models as core_models
 
 # Create your models here.
+
+
+class List(core_models.TimeStampedModel):
+
+    name = models.CharField(max_length=80, null=True)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE, null=True)
+    rooms = models.ManyToManyField("rooms.Room", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
--- rooms/models.py
+++ rooms/models.py
@@ -1,12 +1,66 @@
 from typing import Tuple
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.deletion import CASCADE
 from core import models as core_models
 from django_countries.fields import CountryField
 from users import models as user_models
 
 # Create your models here.
+
+
+class AbstractItem(core_models.TimeStampedModel):
+    name = models.CharField(max_length=80, blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+    pass
+
+    class Meta:
+        verbose_name = "Room Type"
+
+
+class Amenity(AbstractItem):
+    pass
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstractItem):
+    pass
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+
+class HouseRule(AbstractItem):
+    pass
+
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampedModel):
+    caption = models.CharField(max_length=80, null=True, blank=True)
+    file = models.ImageField(null=True, blank=True)
+    room = models.ForeignKey("Room", on_delete=CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.caption
+
+
 class Room(core_models.TimeStampedModel):
-    name = models.CharField(max_length=120, null=True, blank=True)
+    name = models.CharField(
+        max_length=120,
+        null=True,
+    )
     description = models.TextField(null=True, blank=True)
     country = CountryField(null=True, blank=True)
     city = models.CharField(max_length=80, null=True, blank=True)
@@ -20,8 +74,14 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField(null=True, blank=True)
     instant_book = models.BooleanField(default=False, null=True, blank=True)
     host = models.ForeignKey(
-        user_models.User, on_delete=models.CASCADE, null=True, blank=True
+        "users.User", on_delete=models.CASCADE, null=True, blank=True
     )
+    room_type = models.ForeignKey(
+        "RoomType", on_delete=models.SET_NULL, blank=True, null=True
+    )
+    amenity = models.ManyToManyField("Amenity", blank=True, null=True)
+    facility = models.ManyToManyField("Facility", blank=True, null=True)
+    house_rule = models.ManyToManyField("HouseRule", blank=True, null=True)
 
     def __str__(self):
         return self.name
